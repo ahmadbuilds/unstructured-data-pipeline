05fb7ec16aa5a6caa34ae4b724c1fa26083fd8ca
Ahmad <crisitiano678@gmail.com>
2026-01-03 21:33:12 +0500

    Updated Schema

--- src/unstructured-data-pipeline/domain/schema/invoice.py
+++ src/unstructured-data-pipeline/domain/schema/invoice.py
@@ -1,8 +1,8 @@
-from pydantic import BaseModel
-from pydantic import Field
+from pydantic import BaseModel,Field
 import datetime
-
-class ItemDetails(BaseModel):
+from typing_extensions import TypedDict
+from typing import List
+class ItemDetails(TypedDict):
     Description:str=Field("Details about the Item")
     Quantity:str=Field("How much the item is required by the user")
     Unit_price:int=Field("Price of one item")
@@ -10,6 +10,6 @@ class ItemDetails(BaseModel):
 class Invoice(BaseModel):
     Sender_Name:str=Field(description="Invoice Sender Name Information")
     Date:datetime.date=Field(description="Date of the Invoice")
-    Item_Details:ItemDetails
+    Item_Details:List[ItemDetails]
     Total_Amount:float=Field(description="Total Amount of the Sender")
     Currency:str=Field(description="Currency Name")

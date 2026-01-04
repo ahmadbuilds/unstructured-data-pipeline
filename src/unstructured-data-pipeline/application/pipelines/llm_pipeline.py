b9edcae99bf365bba1cfb14cc89c13c42d29c6c6
Ahmad <crisitiano678@gmail.com>
2026-01-04 22:25:10 +0500

    Complete Agent Pipeline

---
+++ src/unstructured-data-pipeline/application/pipelines/llm_pipeline.py
@@ -0,0 +1,18 @@
+from domain.prompts.llm_instructions import template
+from domain.schema.invoice import Invoice,ItemDetails
+from domain.schema.llm_output import response_output
+from domain.tools.file_creation import db_file_creation,check_DBfile_existence
+from domain.tools.sql_execution import manipulating_DBTable
+from infrastructure.llm_providers.llama_provider import create_ollama_instance
+from langchain.agents import create_agent
+from langchain.agents.structured_output import ToolStrategy
+
+def create_agent_instance():
+    agent=create_agent(
+        model=create_ollama_instance(),
+        system_prompt=template,
+        tools=[db_file_creation,check_DBfile_existence,manipulating_DBTable],
+        response_format=ToolStrategy(response_output)
+    )
+
+    return agent

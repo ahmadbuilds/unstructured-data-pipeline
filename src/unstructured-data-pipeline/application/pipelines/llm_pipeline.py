51e3be8acfcd2dc5ae1c44228dac7cce474aec12
Ahmad <crisitiano678@gmail.com>
2026-01-04 23:38:11 +0500

    Added Memory to the Agent

--- src/unstructured-data-pipeline/application/pipelines/llm_pipeline.py
+++ src/unstructured-data-pipeline/application/pipelines/llm_pipeline.py
@@ -6,13 +6,17 @@ from domain.tools.sql_execution import manipulating_DBTable
 from infrastructure.llm_providers.llama_provider import create_ollama_instance
 from langchain.agents import create_agent
 from langchain.agents.structured_output import ToolStrategy
+from langgraph.checkpoint.memory import InMemorySaver
+
+checkPointer=InMemorySaver()
 
 def create_agent_instance():
     agent=create_agent(
         model=create_ollama_instance(),
         system_prompt=template,
         tools=[db_file_creation,check_DBfile_existence,manipulating_DBTable],
-        response_format=ToolStrategy(response_output)
+        response_format=ToolStrategy(response_output),
+        checkpointer=checkPointer
     )
 
     return agent

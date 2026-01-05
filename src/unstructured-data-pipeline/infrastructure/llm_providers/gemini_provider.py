9754143bd8e981a4ab996adc6826e96afeb61d5d
Ahmad <crisitiano678@gmail.com>
2026-01-05 22:12:12 +0500

    LLM Provider updated

--- src/unstructured-data-pipeline/infrastructure/llm_providers/gemini_provider.py
+++ src/unstructured-data-pipeline/infrastructure/llm_providers/gemini_provider.py
@@ -1,7 +1,7 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
 from config import GEMINI_API_KEY
 
-def create_gemini_instance(model_name:str="gemini-2.5-flash",temperature:float=0):
+def create_gemini_instance(model_name:str="gemini-2.5-flash-lite",temperature:float=0):
     model=ChatGoogleGenerativeAI(
         model=model_name,
         google_api_key=GEMINI_API_KEY,

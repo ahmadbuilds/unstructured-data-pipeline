b8a1f8cfb63c360be55f0965cd6277fc0994f9fb
Ahmad <crisitiano678@gmail.com>
2026-01-05 12:06:30 +0500

    Shifted to ReAct Agents

--- src/unstructured-data-pipeline/application/pipelines/llm_pipeline.py
+++ src/unstructured-data-pipeline/application/pipelines/llm_pipeline.py
@@ -4,14 +4,14 @@ from domain.schema.llm_output import response_output
 from domain.tools.file_creation import db_file_creation,check_DBfile_existence
 from domain.tools.sql_execution import manipulating_DBTable
 from infrastructure.llm_providers.llama_provider import create_ollama_instance
-from langchain.agents import create_agent
+from langchain.agents import create_react_agent
 from langchain.agents.structured_output import ToolStrategy
 from langgraph.checkpoint.memory import InMemorySaver
 
 checkPointer=InMemorySaver()
 
 def create_agent_instance():
-    agent=create_agent(
+    agent=create_react_agent(
         model=create_ollama_instance(),
         system_prompt=template,
         tools=[db_file_creation,check_DBfile_existence,manipulating_DBTable],

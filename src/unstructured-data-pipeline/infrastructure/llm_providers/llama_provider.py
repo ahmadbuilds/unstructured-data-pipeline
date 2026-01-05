aeafd281d03d3f7f8b09f37cdf41b6efe4d2c062
Ahmad <crisitiano678@gmail.com>
2026-01-05 20:12:17 +0500

    Updated COnfiguration File

--- src/unstructured-data-pipeline/infrastructure/llm_providers/llama_provider.py
+++
@@ -1,8 +0,0 @@
-from langchain_ollama import ChatOllama
-
-def create_ollama_instance(model_name:str="llama3.1:8b",temperature:float=0):
-    model=ChatOllama(
-        model=model_name,
-        temperature=temperature,
-    )
-    return model

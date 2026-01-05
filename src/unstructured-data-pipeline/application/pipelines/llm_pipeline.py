593d65bff6e01767eaed5c8d4f9bbedc966d290d
Ahmad <crisitiano678@gmail.com>
2026-01-05 22:13:16 +0500

    LLM pipeline updated

--- src/unstructured-data-pipeline/application/pipelines/llm_pipeline.py
+++ src/unstructured-data-pipeline/application/pipelines/llm_pipeline.py
@@ -7,14 +7,15 @@ from infrastructure.llm_providers.gemini_provider import create_gemini_instance
 from langchain.agents import create_agent
 from langchain.agents.structured_output import ToolStrategy
 from langgraph.checkpoint.memory import InMemorySaver
-from domain.schema.invoice import Invoice,ItemDetails
+import json
 
 checkPointer=InMemorySaver()
 
 def create_agent_instance():
+    formatted_template=format_template(Invoice,ItemDetails)
     agent=create_agent(
         model=create_gemini_instance(),
-        system_prompt=template.prompt.template,
+        system_prompt=formatted_template,
         tools=[db_file_creation,check_DBfile_existence,manipulating_DBTable],
         response_format=ToolStrategy(response_output),
         checkpointer=checkPointer,
@@ -23,11 +24,10 @@ def create_agent_instance():
     return agent
 
 
-def format_template(main_schema:Invoice,foreign_schema:ItemDetails,content:str,output_db_path:str=None)->str:
+def format_template(main_schema:Invoice,foreign_schema:ItemDetails)->str:
+
     formatted_template=template.format(
-        main_schema=main_schema,
-        foreign_schema=foreign_schema,
-        content=content,
-        output_db_path=output_db_path,
+        primary_schema=json.dumps(main_schema.model_json_schema(),indent=2),
+        foreign_schema=json.dumps(foreign_schema.model_json_schema(),indent=2)
     )
     return formatted_template
